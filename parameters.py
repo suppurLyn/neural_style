3c8281fc48993aa47a230fd96cec268f237a8284
supperLyn <819092555@qq.com>
2017-01-18 20:38:36 +0800

    first commit

---
+++ parameters.py
@@ -0,0 +1,36 @@
+python neural_style.py  --content examples/c1.jpg --styles  examples/s1.jpg  --output output/1.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c2.jpg --styles  examples/s1.jpg  --output output/2.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s1.jpg  --output output/3.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s1.jpg  --output output/4.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c1.jpg --styles  examples/s2.jpg  --output output/11.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c2.jpg --styles  examples/s2.jpg  --output output/22.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s2.jpg  --output output/33.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s2.jpg  --output output/44.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c1.jpg --styles  examples/s3.jpg  --output output/111.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c2.jpg --styles  examples/s3.jpg  --output output/222.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s3.jpg  --output output/333.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/444.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c1.jpg --styles  examples/s4.jpg  --output output/1111.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c2.jpg --styles  examples/s4.jpg  --output output/2222.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s4.jpg  --output output/3333.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s4.jpg  --output output/4444.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+
+
+python neural_style.py  --content examples/c3.jpg --styles  examples/s2.jpg  --output output/11111.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/22222.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s4.jpg  --output output/33333.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s2.jpg  --output output/44444.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s3.jpg  --output output/a.jpg --learning-rate 8  --style-weight 130 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s4.jpg  --output output/b.jpg --learning-rate 8  --style-weight 130 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s2.jpg  --output output/c.jpg --learning-rate 8  --style-weight 130 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/d.jpg --learning-rate 10  --style-weight 150 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s4.jpg  --output output/e.jpg --learning-rate 10  --style-weight 150 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/f.jpg --learning-rate 12  --style-weight 150 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c3.jpg --styles  examples/s4.jpg  --output output/g.jpg --learning-rate 12  --style-weight 150 --iterations 2000 --print-iterations 2000;
+
+
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/22222.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s2.jpg  --output output/44444.jpg --learning-rate 10  --style-weight 100 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s4.jpg  --output output/b.jpg --learning-rate 8  --style-weight 130 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/d.jpg --learning-rate 10  --style-weight 150 --iterations 2000 --print-iterations 2000;
+python neural_style.py  --content examples/c4.jpg --styles  examples/s3.jpg  --output output/f.jpg --learning-rate 12  --style-weight 150 --iterations 2000 --print-iterations 2000;
